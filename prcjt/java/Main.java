c1a20cbd0fe80556b870b3609feb24285a7bd79a
kirillDog <kirilldogadin1993@gmail.com>
2017-10-11 11:38:59 +0300

    test jdk

--- prcjt/java/Main.java
+++ prcjt/java/Main.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main(String[] args) {
+        Predicate<Integer> atLeast5 = x -> x > 5;
+        Stream.of("a", "b", "hello")
+                .map(s -> s.toUpperCase())
+                .forEach(System.out::println);
+
+    }
 }
